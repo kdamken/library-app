7f77140750ed7c3e2a5e18c2925087f5189bb165
Kyle Damken <kyledamken@Kyles-MBP.fios-router.home>
2023-11-13 14:40:07 -0500

    add some more packages

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 require('dotenv').config()
+var debug = require('debug')('app');
+const compression = require("compression");
+const helmet = require("helmet");
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var stylus = require('stylus');
+// Set up rate limiter: maximum of twenty requests per minute
+const RateLimit = require("express-rate-limit");
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -16,17 +21,33 @@ var app = express();
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
 
-main().catch((err) => console.log(err));
+main().catch((err) => debug.log(err));
 async function main() {
   await mongoose.connect(process.env.MONGODB_CONNECTION_STRING);
 }
 
-console.log('hello');
+debug('hello');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+const limiter = RateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 20,
+});
+// Apply rate limiter to all requests
+app.use(limiter);
+// Add helmet to the middleware chain.
+// Set CSP headers to allow our Bootstrap and Jquery to be served
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      "script-src": ["'self'", "code.jquery.com", "cdn.jsdelivr.net"],
+    },
+  }),
+);
+app.use(compression()); // Compress all routes
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
